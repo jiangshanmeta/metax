6fb0fb0e81081bdde7f04e1a83f623c863c30318
jiangshanmeta <540118044@qq.com>
2017-11-25 10:01:45 +0800

    lifecycle

---
+++ src/lifecycle.js
@@ -0,0 +1,32 @@
+function init_lifecycle(store){
+    const options = store.$options;
+    const parent = options.parent;
+    store.$parent = parent;
+    if(parent){
+        store.$root = parent.$root
+    }else{
+        store.$root = store;
+    }
+
+    if(!options.modulePath){
+        options.modulePath = [];
+    }
+
+
+}
+
+function get_first_unnamespaced_parent(store){
+    let parent = store.$parent;
+    while(parent){
+        if(parent.$options.namespaced){
+            return parent;
+        }
+        parent = parent.$parent
+    }
+    return store.$root;
+}
+
+export {
+    init_lifecycle,
+    get_first_unnamespaced_parent,
+}
